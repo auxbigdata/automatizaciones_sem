2ea7b602a530edb4eb6a47408e22dc5e0f2368a0
produccion_robots <auxanalista@gmail.com>
2026-05-22 10:22:19 -0500

    actualizacion entorno y cambio de pass_aplicacion emsa

--- src/settings/entorno.py
+++ src/settings/entorno.py
@@ -12,20 +12,6 @@ class Entorno(BaseSettings):
     URL_HOME_COFREM: str
     URL_REPORTE_COFREM: str
     URL_SGC_SUBIR_ARCHIVO_COFRES: str
-    URL_GRILLA: str
-    URL_FINALIZAR_EMSA: str
-    URL_DESCARTAR_EMSA: str
-    URL_HOME_EMSA: str
-    URL_DESCARGAR_PDF_EMSA: str
-    URL_COMPARTIDA: str
-    IP_COMPARTIDA: str
-    USER_COMPARTIDA: str
-    PASS_COMPARTIDA: str
-    DB_HOST: str
-    DB_PORT: int
-    DB_NAME: str
-    DB_USER: str
-    DB_PASS: str
 
 # Instancia global lista para ser importada
 env = Entorno()
